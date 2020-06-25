9d6991b67d7ca52acb8095e4ab340653453b82d0
hoangbaoa <hoangbao1223@gmail.com>
2020-06-25 22:47:17 +0700

    Flasher: anykernel.sh: Set correct permissions for included ramdisk files
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- Flasher/anykernel.sh
+++ Flasher/anykernel.sh
@@ -27,8 +27,7 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 750 750 $ramdisk/*;
 
 
 ## AnyKernel boot install
