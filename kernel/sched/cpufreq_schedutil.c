d2af427c05c05fb50f6c7313086538999e2dd29c
Lau <laststandrighthere@gmail.com>
2020-07-31 04:52:15 +0800

    Revert "cpufreq: schedutil: Don't jump to max frequency for RT tasks"
    This is no longer needed since the previous commit 90df74e ("ANDROID:
    sched: cpufreq: use rt_avg as estimate of required RT CPU capacity")
    
    This reverts commit 76ef171c1b636794e7f6a3f8a44461e9ade62139.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -318,8 +318,6 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	unsigned int next_f;
 	bool busy;
 
-	flags &= ~SCHED_CPUFREQ_RT_DL;
-
 	if (flags & SCHED_CPUFREQ_PL)
 		return;
 
@@ -410,8 +408,6 @@ static void sugov_update_shared(struct update_util_data *hook, u64 time,
 		return;
 
 	sugov_get_util(&util, &max, sg_cpu->cpu);
-
-	flags &= ~SCHED_CPUFREQ_RT_DL;
 
 	raw_spin_lock(&sg_policy->update_lock);
 
