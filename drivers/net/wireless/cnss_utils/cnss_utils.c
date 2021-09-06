5cedc4a6a52bd8627f90e0c9890e7a1e62dd31b6
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-09-06 12:21:08 +0700

    cnss_utils: Don't create debugfs when CONFIG_DEBUG_FS=n
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- drivers/net/wireless/cnss_utils/cnss_utils.c
+++ drivers/net/wireless/cnss_utils/cnss_utils.c
@@ -16,7 +16,9 @@
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/etherdevice.h>
+#ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
+#endif
 #include <net/cnss_utils.h>
 
 #define CNSS_MAX_CH_NUM 45
@@ -433,6 +435,7 @@ static const struct file_operations cnss_utils_mac_fops = {
 	.llseek		= seq_lseek,
 };
 
+#ifdef CONFIG_DEBUG_FS
 static int cnss_utils_debugfs_create(struct cnss_utils_priv *priv)
 {
 	int ret = 0;
@@ -451,6 +454,7 @@ static int cnss_utils_debugfs_create(struct cnss_utils_priv *priv)
 out:
 	return ret;
 }
+#endif
 
 static int __init cnss_utils_init(void)
 {
@@ -464,7 +468,9 @@ static int __init cnss_utils_init(void)
 
 	mutex_init(&priv->unsafe_channel_list_lock);
 	spin_lock_init(&priv->dfs_nol_info_lock);
+#ifdef CONFIG_DEBUG_FS
 	cnss_utils_debugfs_create(priv);
+#endif
 	cnss_utils_priv = priv;
 
 	return 0;
