9712d22de4770e592f58511fa31946234303ad49
Divyanshu-Modi <divyan.m05@gmail.com>
2021-09-14 10:10:51 +0530

    techpack: audio: apr: silence ipc logging when ipc logging is disabled
     Silences:
      [    3.274652] apr_probe: Unable to create ipc log context
    
    Change-Id: I366f89a01c27d0718972d433e8c76d543bf1b549
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- techpack/audio/ipc/apr.c
+++ techpack/audio/ipc/apr.c
@@ -1153,10 +1153,12 @@ static int apr_probe(struct platform_device *pdev)
 	if (!apr_reset_workqueue)
 		return -ENOMEM;
 
+#ifdef CONFIG_IPC_LOGGING
 	apr_pkt_ctx = ipc_log_context_create(APR_PKT_IPC_LOG_PAGE_CNT,
 						"apr", 0);
 	if (!apr_pkt_ctx)
 		pr_err("%s: Unable to create ipc log context\n", __func__);
+#endif
 
 	is_initial_boot = true;
 	subsys_notif_register("apr_adsp", AUDIO_NOTIFIER_ADSP_DOMAIN,
