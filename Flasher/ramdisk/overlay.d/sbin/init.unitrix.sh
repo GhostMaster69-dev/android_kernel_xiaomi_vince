d6b0294951b3d23924ecb5a2f564e76575db305b
GhostMaster69-dev <rathore6375@gmail.com>
2021-09-29 12:36:17 +0530

    Flasher: Add tuneable initrd profile script for unitrix
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

---
+++ Flasher/ramdisk/overlay.d/sbin/init.unitrix.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+
+# Schedutil Governor Settings
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1401600 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_freq
+echo 85 > /sys/devices/system/cpu/cpufreq/schedutil/hispeed_load
+echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/pl
+echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
+echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+echo 652800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
