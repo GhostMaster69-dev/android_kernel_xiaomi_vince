803879337f6261f8be06f48ba9da10f1ad2ea3a2
GhostMaster69-dev <rathore6375@gmail.com>
2021-10-01 22:05:41 +0530

    Revert "BACKPORT: netfilter: x_tables: cap allocations at 512 mbyte"
    This reverts commit 86f119a5ecda1ce50982e17f1e98c3a2065cb706.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- net/netfilter/x_tables.c
+++ net/netfilter/x_tables.c
@@ -40,7 +40,6 @@ MODULE_AUTHOR("Harald Welte <laforge@netfilter.org>");
 MODULE_DESCRIPTION("{ip,ip6,arp,eb}_tables backend module");
 
 #define XT_PCPU_BLOCK_SIZE 4096
-#define XT_MAX_TABLE_SIZE       (512 * 1024 * 1024)
 
 struct compat_delta {
 	unsigned int offset; /* offset in kernel */
@@ -1035,7 +1034,7 @@ struct xt_table_info *xt_alloc_table_info(unsigned int size)
 	struct xt_table_info *info = NULL;
 	size_t sz = sizeof(*info) + size;
 
-	if (sz < sizeof(*info) || sz >= XT_MAX_TABLE_SIZE)
+	if (sz < sizeof(*info))
 		return NULL;
 
 	/* Pedantry: prevent them from hitting BUG() in vmalloc.c --RR */
