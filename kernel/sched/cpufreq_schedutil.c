bcfc8e5541052116358e2605036450102c082e51
GhostMaster69-dev <rathore6375@gmail.com>
2021-10-14 10:38:09 +0530

    Revert "schedutil: tune default high speed load and high speed freq"
    This reverts commit 2bebe21c59907bf3959bd41fe1660b6f118a8796.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -302,7 +302,7 @@ static void sugov_calc_avg_cap(struct sugov_policy *sg_policy, u64 curr_ws,
 }
 
 #define NL_RATIO 75
-#define DEFAULT_HISPEED_LOAD 85
+#define DEFAULT_HISPEED_LOAD 90
 static void sugov_walt_adjust(struct sugov_cpu *sg_cpu, unsigned long *util,
 			      unsigned long *max)
 {
@@ -910,7 +910,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->up_rate_limit_us = 0;
 	tunables->down_rate_limit_us = 0;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
-	tunables->hispeed_freq = 1401600;
+	tunables->hispeed_freq = 0;
 
 	tunables->iowait_boost_enable = false;
 
