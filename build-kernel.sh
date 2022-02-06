24f3b707aa0f7ef3981f6c7bfba0fdebcdaf173d
GhostMaster69-dev <rathore6375@gmail.com>
2022-02-06 06:41:55 +0000

    build-kernel: Rework on kernel building script
    * Remove Miui and GCC derps
    * Remove some unused packages for faster launch
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,51 +2,31 @@
 
 #
 # Copyright (C) 2020 StarLight5234
-# Copyright (C) 2021 GhostMaster69-dev
+# Copyright (C) 2021-22 GhostMaster69-dev
 #
 
-###############################################################
-#==================== Unitrix-Kernel ==========================
-###############################################################
 export DEVICE="VINCE"
 export CONFIG="vince-perf_defconfig"
 export TC_PATH="$HOME/toolchains"
 export ZIP_DIR="$(pwd)/Flasher"
-export IS_MIUI="no"
 export KERNEL_DIR=$(pwd)
 export KBUILD_BUILD_USER="Unitrix-Kernel"
-export GCC_COMPILE="no"
 export KBUILD_BUILD_HOST="Cosmic-Horizon"
 export KBUILD_COMPILER_STRING="Cosmic clang version 14.0.0"
-###############################################################
-#==================== Unitrix-Kernel ==========================
-###############################################################
 
-###############################################################
-#===================== Telegram Bot API Token =================
-###############################################################
-#===================== Telegram Channel ID ====================
-###############################################################
-
-# Ask TG Channel ID
+# Ask Telegram Channel/Chat ID
 if [[ -z ${CHANNEL_ID} ]]; then
     echo -n "Plox,Give Me Your TG Channel/Group ID:"
     read -r tg_channel_id
     CHANNEL_ID="${tg_channel_id}"
 fi
 
-# Ask TG Bot Token
+# Ask Telegram Bot API Token
 if [[ -z ${TELEGRAM_TOKEN} ]]; then
     echo -n "Plox,Give Me Your TG Bot API Token:"
     read -r tg_token
     TELEGRAM_TOKEN="${tg_token}"
 fi
-
-###############################################################
-#==================== Function Definition =====================
-###############################################################
-#======================= Telegram Start =======================
-###############################################################
 
 # Upload buildlog to group
 tg_erlog()
@@ -87,50 +67,27 @@ function error_sticker() {
         -d sticker="$STICKER" \
         -d chat_id=$CHANNEL_ID
 }
-
-###############################################################
-#======================= Telegram End =========================
-###############################################################
-#========================= Clone TC ===========================
-#======================== & AnyKernel =========================
-###############################################################
-
 function clone_tc() {
 [ -d ${TC_PATH} ] || mkdir ${TC_PATH}
 
-if [ "$GCC_COMPILE" == "no" ]; then
 	git clone -b main --depth=1 https://gitlab.com/GhostMaster69-dev/Cosmic-Clang.git ${TC_PATH}/clang
 	export PATH="${TC_PATH}/clang/bin:$PATH"
 	export STRIP="${TC_PATH}/clang/aarch64-linux-gnu/bin/strip"
 	export COMPILER="Cosmic clang 14.0.0"
-else
-	git clone --depth=1 https://github.com/arter97/arm64-gcc ${TC_PATH}/gcc64
-	git clone --depth=1 https://github.com/arter97/arm32-gcc ${TC_PATH}/gcc32
-	export PATH="${TC_PATH}/gcc64/bin:${TC_PATH}/gcc32/bin:$PATH"
-	export STRIP="${TC_PATH}/gcc64/aarch64-elf/bin/strip"
-	export COMPILER="Arter97's GCC Compiler" 
-fi
-
 }
 
-###############################################################
-#=========================== Make =============================
-#========================== Kernel ============================
-###############################################################
-
+# Make Kernel
 build_kernel() {
 DATE=`date`
 BUILD_START=$(date +"%s")
 make O=out ARCH=arm64 "$CONFIG"
-
-if [ "$GCC_COMPILE" == "no" ]; then
-	make -j$(nproc --all) O=out \
-			      ARCH=arm64 \
+make -j$(nproc --all) O=out \
+	                      ARCH=arm64 \
 			      CC=clang \
 			      AR=llvm-ar \
 			      AS=llvm-as \
-			      NM=llvm-nm \
 			      LD=ld.lld \
+			      NM=llvm-nm \
 			      OBJCOPY=llvm-objcopy \
 			      OBJDUMP=llvm-objdump \
 			      OBJSIZE=llvm-size \
@@ -144,51 +101,18 @@ if [ "$GCC_COMPILE" == "no" ]; then
 			      HOSTLD=ld.lld \
 			      CROSS_COMPILE=aarch64-linux-gnu- \
 			      CROSS_COMPILE_ARM32=arm-linux-gnueabi- |& tee -a $HOME/build/build${BUILD}.txt
-else
-	make -j$(nproc --all) O=out \
-			      ARCH=arm64 \
-			      CROSS_COMPILE=aarch64-elf- \
-			      CROSS_COMPILE_ARM32=arm-eabi- |& tee -a $HOME/build/build${BUILD}.txt
-fi
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 }
 
-###############################################################
-#==================== Make Flashable Zip ======================
-###############################################################
-
 function make_flashable() {
-
-if [ "$IS_MIUI" == "yes" ]; then
-# credit @adekmaulana
-    for MODULES in $(find "$KERNEL_DIR/out" -name '*.ko'); do
-        "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
-        "$KERNEL_DIR/scripts/sign-file" sha512 \
-                "$KERNEL_DIR/out/signing_key.priv" \
-                "$KERNEL_DIR/out/signing_key.x509" \
-                "${MODULES}"
-        case ${MODULES} in
-                */wlan.ko)
-		cp "${MODULES}" "${VENDOR_MODULEDIR}/pronto_wlan.ko"
-            ;;
-        esac
-    done
-    echo -e "(i) Done moving wifi modules"
-fi
 
 cd $ZIP_DIR
 make clean &>/dev/null
-cp $KERN_IMG $ZIP_DIR/zImage
+cp $KERN_IMG $ZIP_DIR
 if [ "$BRANCH" == "stable" ]; then
 	make stable &>/dev/null
-elif [ "$BRANCH" == "stable-perf" ]; then
-	make stable &>/dev/null
-elif [ "$BRANCH" == "beta" ]; then
-	make beta &>/dev/null
-elif [ "$BRANCH" == "beta-perf" ]; then
-	make beta &>/dev/null
 else
 	make test &>/dev/null
 fi
@@ -196,10 +120,6 @@ ZIP=$(echo *.zip)
 tg_pushzip
 
 }
-
-###############################################################
-#========================= Build Log ==========================
-###############################################################
 
 # Credits: @madeofgreat
 BTXT="$HOME/build/buildno.txt" #BTXT is Build number TeXT
@@ -212,11 +132,6 @@ fi
 BUILD=$(cat $BTXT)
 BUILD=$(($BUILD + 1))
 echo ${BUILD} > $BTXT
-
-###############################################################
-#===================== Random sticker =========================
-#==================== for build error =========================
-###############################################################
 
 stick=$(($RANDOM % 5))
 
@@ -232,11 +147,6 @@ elif [ "$stick" == "4" ];then
 	STICKER="CAACAgUAAxkBAAMUXveDrb4guQZSu7mP7ZptE4547PsAAugAA_scAAFXWZ-1a2wWKUcaBA"
 fi
 
-###############################################################
-#===================== End of function ========================
-#======================= definition ===========================
-###############################################################
-
 clone_tc
 
 COMMIT=$(git log --pretty=format:'"%h : %s"' -1)
@@ -245,7 +155,7 @@ KERNEL_DIR=$(pwd)
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 CONFIG_PATH=$KERNEL_DIR/arch/arm64/configs/$CONFIG
 VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
-export KERN_VER=$(echo "$(make kernelversion)")
+export KERN_VER=$(echo "$(make --no-print-directory kernelversion)")
 
 # Cleaning source
 make mrproper && rm -rf out
@@ -254,9 +164,9 @@ start_sticker
 tg_sendinfo "$(echo -e "======= <b>$DEVICE</b> =======\n
 Build-Host   :- <b>$KBUILD_BUILD_HOST</b>
 Build-User   :- <b>$KBUILD_BUILD_USER</b>\n 
-Version        :- <u><b>$KERN_VER</b></u>
-Compiler      :- <i>$COMPILER</i>\n
-on Branch   :- <b>$BRANCH</b>
+Version      :- <u><b>$KERN_VER</b></u>
+Compiler     :- <i>$COMPILER</i>\n
+on Branch    :- <b>$BRANCH</b>
 Commit       :- <b>$COMMIT</b>\n")"
 
 build_kernel
