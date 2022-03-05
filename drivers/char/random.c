0ad54810605408d98f7953933f8c7fc498d884a9
GhostMaster69-dev <rathore6375@gmail.com>
2022-03-05 03:54:23 +0000

    char: random: silence unused function warning
    ../drivers/char/random.c:1761:1: warning: unused function 'random_read' [-Wunused-function]
    random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
    ^
    1 warning generated.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1757,11 +1757,13 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 	}
 }
 
+#ifndef CONFIG_SRANDOM
 static ssize_t
 random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
 }
+#endif
 
 static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
