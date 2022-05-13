3211a69bbdb2e08342f29d8055ac1bbe594ba8f4
GhostMaster69-dev <rathore6375@gmail.com>
2022-05-13 12:15:21 +0000

    tty: msm_smd_tty: Make Init function for IPC logging a no-op
    drivers/tty/serial/msm_smd_tty.c:827:13: warning: unused function 'smd_tty_log_init' [-Wunused-function]
    static void smd_tty_log_init(void)
                ^
    1 warning generated.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- drivers/tty/serial/msm_smd_tty.c
+++ drivers/tty/serial/msm_smd_tty.c
@@ -818,6 +818,7 @@ static struct notifier_block smd_tty_pm_nb = {
 	.priority = 0,
 };
 
+#ifdef CONFIG_IPC_LOGGING
 /**
  * smd_tty_log_init()- Init function for IPC logging
  *
@@ -831,6 +832,7 @@ static void smd_tty_log_init(void)
 	if (!smd_tty_log_ctx)
 		pr_err("%s: Unable to create IPC log", __func__);
 }
+#endif
 
 static struct tty_driver *smd_tty_driver;
 
