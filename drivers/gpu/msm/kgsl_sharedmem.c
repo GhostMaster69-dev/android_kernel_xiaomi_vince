67f63892c1d85367b1d5798409319ca4dcc8c74c
GhostMaster69-dev <rathore6375@gmail.com>
2022-07-10 15:27:59 +0000

    Revert "msm: kgsl: Fix memory leak for anonymous buffers"
    This reverts commit 501a3cbad8649bce481b1d455fee4f33b4e49a1e.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -996,7 +996,6 @@ void kgsl_sharedmem_free(struct kgsl_memdesc *memdesc)
 	if (memdesc->sgt) {
 		sg_free_table(memdesc->sgt);
 		kfree(memdesc->sgt);
-		memdesc->sgt = NULL;
 	}
 
 	memdesc->page_count = 0;
