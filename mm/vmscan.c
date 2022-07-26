df787951945624664198b3601c4e34ca1826194c
GhostMaster69-dev <rathore6375@gmail.com>
2022-07-26 17:18:44 +0000

    mm: set vm_swappiness to 50% of max value
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -148,9 +148,9 @@ struct scan_control {
 #endif
 
 /*
- * From 0 .. 100.  Higher means more swappy.
+ * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 100;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
